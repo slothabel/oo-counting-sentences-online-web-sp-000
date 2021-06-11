28acf41ee0fd9c4e2777a49380b89bb7d46947ee
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-06-11 16:44:25 -0700

    commit tree eaecb2460e2387a123da7ca51d9d37d101e41da6

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,14 +11,27 @@ class String
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+   else
+    false
+   end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+   else
+    false
+   end
   end
 
   def count_sentences
+    new_array = self.split(/[.*!*?*]/)
+    no_empty = new_array.reject {|i| i.empty?}
+    no_empty.count
 
+    #binding.pry
   end
+
 end
